05db8b75c46df7670df206632c022caf37f938f0
divyadivyadekonda-lang <divyadivyadekonda@gmail.com>
2026-06-09 20:56:03 +0530

    add variables.java

---
+++ variables.java
@@ -0,0 +1,20 @@
+public class variables
+{
+    public static void main(string[]args)
+    {
+        int x=20;
+        double y=3.50;
+        string z="Divya";
+        boolean W=true;
+        system.out.printLn(x);
+        system.out.println(Y);
+        system.out.printLn(Z);
+        system.out.printLn(w);
+    }
+}
+
+    
+    
+
+
+
