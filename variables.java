52964b41101cc3d5a6880b70b898b3f0b5809a24
divyadivyadekonda-lang <divyadivyadekonda@gmail.com>
2026-06-10 19:37:39 +0530

    add Variables.java

--- variables.java
+++ variables.java
@@ -1,11 +1,11 @@
 public class Variables
 {
-    public static void main(String[]args)
+    public static void main(String[] args)
     {
         int x=20;
         double y=13.0;
         String z="Divya";
-        boolean W=true;
+        boolean w =true;
         System.out.println(x);
         System.out.println(y);
         System.out.println(z);
