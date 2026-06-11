d7ac83ed43aaf141efd69560be6d228c27be22df
divyadivyadekonda-lang <divyadivyadekonda@gmail.com>
2026-06-11 15:12:29 +0530

    Add Datatype.java

---
+++ Datatype.java
@@ -0,0 +1,36 @@
+public class Datatype
+{
+    public static void main(String[] args)
+    
+    {
+    byte age =50;
+    short bookPages=700;
+    int marks=90;
+    long aadharNO=898851427729;
+    float weight=50.5f;
+    double bankBalance=40000; 
+    char section='A';
+    boolean isopen=true;
+    System.out.printLN(age);
+    System.out.printLN(bookPages);
+    System.out.printLn(marks);
+    System.out.printLn(aadharNO);
+    System.out,printLn(weight);
+    System.out.printLn(bankBalance);
+    System.out.printLn(section);
+    System.out.printLN(isopen);
+}
+
+    
+}
+
+    
+
+    
+
+
+
+    
+
+
+
--- divya.java
+++ divya.java
@@ -1,8 +1,8 @@
     public class Divya
     {
-        public static void main(string[]args)
-System.out.printLn("divya");
-    }
+        public static v
+
+    
 
     
 
