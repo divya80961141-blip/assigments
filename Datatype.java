13d298cdcf08f9e97c4a452db5ffdb3fdd1c6ba7
divyadivyadekonda-lang <divyadivyadekonda@gmail.com>
2026-06-11 15:33:59 +0530

    add Datatype.java

--- Datatype.java
+++ Datatype.java
@@ -6,7 +6,7 @@ public class Datatype
     byte age =50;
     short bookPages=700;
     int marks=90;
-    long aadharNO=898851427729;
+    long adharNO=898851427729;
     float weight=50.5f;
     double bankBalance=40000; 
     char section='A';
@@ -15,7 +15,7 @@ public class Datatype
     System.out.printLN(bookPages);
     System.out.printLn(marks);
     System.out.printLn(aadharNO);
-    System.out,printLn(weight);
+    System.out.printLn(weight);
     System.out.printLn(bankBalance);
     System.out.printLn(section);
     System.out.printLN(isopen);
