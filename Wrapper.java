b6bcb4745136932c160715634a406e2b8ce9cac1
divyadivyadekonda-lang <divyadivyadekonda@gmail.com>
2026-06-12 17:20:52 +0530

    add Wrapper.java

---
+++ Wrapper.java
@@ -0,0 +1,30 @@
+public class Wrapper
+{
+public static void main(String[] args){
+System.out.printLn(Byte.MIN.VALUE);
+System.out.printLn(Byte.MAX.VALUE);
+System.out.printLn(Byte.SIZE);
+System.out.printLn(Short.MIN.VALUE);
+System.out.printLn(Short.MAX.VALUE);
+System.out.printLn(Short.SIZE);
+System.out.printLn(Integer.MIN.VALUE);
+System.out.printLn(Integer.MAX.VALUE);
+System.out.printLn(Integer.SIZE);
+System.out.printLn(Long.MIN.VALUE);
+System.out.printLn(Long.MAX.VALUE);
+System.out.printLn(Long.SIZE);
+System.out.printLn(Float.MIN.VALUE);
+System.out.printLn(Float.MAX.VALUE);
+System.out.printLn(Float.SIZE);
+System.out.printLn(Double.MIN.VALUE);
+Systen.out.printLn(Double.MAX.VALUE);
+System.out.printLn(Double.SIZE);
+System.out.printLn(Boolean.MIN.VALUE);
+System.out.printLn(Boolean.MAX.VALUES);
+System.out.printLn(Boolean.SIZE);
+System.out.printLn(Character.MIN.VALUE);
+System.out.printLn(Character.MAX.VALUE);
+System.out.printLn(Character.SIZE);
+
+}
+}
