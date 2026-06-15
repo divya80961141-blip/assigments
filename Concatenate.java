c1ea708d3601b62a4db47d711f6aa3701330c353
divyadivyadekonda-lang <divyadivyadekonda@gmail.com>
2026-06-15 16:30:24 +0530

    add v=Variables2.java

---
+++ Concatenate.java
@@ -0,0 +1,17 @@
+public class Concatenate
+{
+    Public static void main(String[] args)
+    
+{
+byte marks=20;
+short handBag=15000;
+int tv=85000;
+long mobile=1500000;
+float sugar=5.2f;
+double waterTank=4.5267;
+char grade='B';
+boolean pass=true;
+System.out.printLn("MY name is": );
+System.out.printLn()
+
+
---
+++ Variables2.java
@@ -0,0 +1,16 @@
+public class variables2{
+    static  double population=2.4444;
+    static double temperature= 3.45;
+}public static void main(String[] args){
+    static double goldprice=98567.5;
+    static double bag=5.66;
+    System.out.printLn(population);
+    System.out.printLn(temperature);
+    System.out.printLn(goldprice);
+    System.out.println(bag);
+}
+
+
+
+    
+ 
