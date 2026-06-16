8c18a76751c3cfa1367ead444aaf387065885b27
divyadivyadekonda-lang <divyadivyadekonda@gmail.com>
2026-06-16 17:41:10 +0530

    ad arithmatic.java

---
+++ arithmatic.java
@@ -0,0 +1,16 @@
+public class arithmatic {
+    public static void main(String[] args)
+    {
+    int a=10;
+    int b=30;
+    System.out.println(a+b);
+    System.out.println(a-b);
+    System.out.println(a*b);
+    System.out.println(a/b);
+    System.out.println(a%b);
+    }
+}
+    
+
+        
+    
