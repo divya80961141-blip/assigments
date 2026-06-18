d3ac6f4b333e56e03fe7cc0343643aa3b5c88975
divyadivyadekonda-lang <divyadivyadekonda@gmail.com>
2026-06-18 16:55:28 +0530

    add decrement.java

---
+++ decrement.java
@@ -0,0 +1,13 @@
+public class decrement {
+    
+public static void main(String[] args)
+{
+   double a=10.5;
+   double x=a++;
+   System.out.println(x);
+   System.out.println(a);
+}
+}
+   
+
+
--- increment.java
+++ increment.java
@@ -1,4 +1,4 @@
-public class incremate{
+public class increment{
     
      public static void main(String[] atgs)
      {
