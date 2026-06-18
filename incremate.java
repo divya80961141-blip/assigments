92ce800622379bdb18b56b7603f707bb1592510a
divyadivyadekonda-lang <divyadivyadekonda@gmail.com>
2026-06-18 16:38:15 +0530

    add incremate.java

---
+++ incremate.java
@@ -0,0 +1,16 @@
+public class incremate{
+    
+     public static void main(String[] atgs)
+     {
+     int x=10;
+      int y= ++x;    
+     System.out.println(y);
+     System.out.println(x);
+     
+
+
+
+    
+}
+}
+    
---
+++ logical.java
@@ -0,0 +1,10 @@
+public class logical {
+   { public static void main(String[] args)
+    int x=10;
+        y=14;
+        System.out.println(x&&y);
+        System.out.println(x||
+
+    
+}
+}
