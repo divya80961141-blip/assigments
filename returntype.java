125bba41a50166a63ef76bed2c7df51c8cab3876
divyadivyadekonda-lang <divyadivyadekonda@gmail.com>
2026-06-29 18:52:37 +0530

    add returntype.java

---
+++ returntype.java
@@ -0,0 +1,39 @@
+public class returntype{
+
+    public static void main(String[] args){
+    name();
+    marks();
+    rollNo();
+
+    }
+
+    public static void name()
+    {
+    System.out.println("divya");
+     }
+    public  static void marks()
+    {
+    System.out.println(40);
+
+    } 
+    public static void rollNo() 
+
+    {
+        
+    System.out.println(6);
+    
+    }
+} 
+
+
+
+
+
+    
+
+
+
+    
+
+
+
