4f44540a9b166119be6bec101fe08996a5615e87
divyadivyadekonda-lang <divyadivyadekonda@gmail.com>
2026-06-30 16:35:24 +0530

    add greet.java

---
+++ greet.java
@@ -0,0 +1,21 @@
+public class greet
+{
+
+  public static void main(String[] args)
+
+  {    
+ send ("hello everyone");
+ text("what are doing");
+
+  }
+   public static void send(String send)
+    {
+
+   System.out.println(send);
+    }
+    public static void text(String text)
+    {
+    System.out.println(text);
+    }
+  
+}
