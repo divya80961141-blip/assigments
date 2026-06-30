d36c1407317c1273b5365c1e324ff0600bbfd964
divyadivyadekonda-lang <divyadivyadekonda@gmail.com>
2026-06-30 17:11:40 +0530

    add methods.java

---
+++ methods.java
@@ -0,0 +1,28 @@
+public class methods {
+    
+
+public static void main(String[] args)
+  {
+    add(50, 20);
+    sub(100, 10);
+    mult(11, 5 );
+    div(1.0, 2.0);
+   }
+  public static int add(int a,int b)
+
+  {
+    return a+b;
+   }
+   public static  int sub(int  a, int b)  
+    {
+    return  a-b;
+   } 
+   public static int  mult (int  a, int  b)
+   {
+    return a*b;
+   }
+  public static double div(double a, double b)
+  {
+    return a/b;
+   }
+}       
