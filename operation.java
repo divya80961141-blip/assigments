adf3f90a379c6a810a889cffed4bba391e4ee612
divyadivyadekonda-lang <divyadivyadekonda@gmail.com>
2026-07-02 21:33:39 +0530

    add operation.java

---
+++ operation.java
@@ -0,0 +1,36 @@
+public class operation {
+
+    
+}
+public static void main(String[] args)
+{
+   int a=50, b=30;
+   //arthmatic operation
+   System.out.println("a+b=" + (a+b));
+   System.out.println("a-b=" + (a-b));
+   System.out.println("a*b=" + (a*b));
+   System.out.println("a/b=" + (a/b));
+   //increment decrment
+   System.out.println("++a=" + (++a));
+   System.out.println("a++=" +(a++));
+   System.out.println("--a=" + (--a));
+   System.out.println("a--=" + (a--));
+   //relation operation
+   
+   System.out.println("a+b="  +(a+b));
+   System.out.println("a-b=" +(a-b));
+   System.out.println("a/b=" +(a/b));
+   System.out.println("a%b=" +(a%b));
+   // composite assigment operation 
+   System.out.println("a+b=" + (a+b));
+   System.out.println("a-b=" +(a-b));
+   System.out.println("a/b=" + (a/b));
+   System.out.println("a%b=" +(a%b));
+ //logical operation
+ System.out.println(a>b && b<=a);
+ System.out.println(a>b || b<=a);
+ System.out.println(a>b | b<a);
+ System.out.println(a<b & a>b);
+
+
+}
